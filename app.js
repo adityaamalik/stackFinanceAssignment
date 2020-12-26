26b71226a83b6d7fa0a0efff163940fcb8e2e9ea
Aditya Malik <aditya007@outlook.com>
2020-12-26 13:29:10 +0530

    home route added

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ app.get("/", function (req, res) {
   res.send("Stack Finance is best !");
 });
 
+app.get("/home", function (req, res) {
+  res.send("Logged in user !");
+});
+
 app.listen(process.env.PORT || "5000", function () {
   console.log("Server is up and running !");
 });
