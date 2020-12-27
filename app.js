5665d2edce7079ed4008ea5fcaf2e20ef0335dc1
Aditya Malik <aditya007@outlook.com>
2020-12-27 18:14:11 +0530

    comments added to the code and records API completed

--- app.js
+++ app.js
@@ -8,9 +8,15 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+//HOME ROUTE
 app.get("/", function (req, res) {
   res.send("Stack Finance is best !");
 });
+
+//GET ALL USERS / GET USER BY USER ID
+
+// @ BODY PARAMS
+// 1. id
 
 app.get("/users", async (req, res) => {
   let url, requestDetails;
@@ -54,6 +60,13 @@ app.get("/users", async (req, res) => {
   }
 });
 
+//ADD A NEW USER
+// BODY PARAMS LIST
+// 1. role_id (GET LIST OF ROLES ID FROM ROLE ROUTE GIVEN BELOW)
+// 2. first_name OF THE USER
+// 3. last_name OF THE USER
+// 4. profile_id (GET LIST OF PROFILES ID FROM PROFILE ROUTE GIVEN BELOW)
+// 5. email OF THE USER
 app.post("/users", async (req, res) => {
   let url = "https://www.zohoapis.in/crm/v2/users";
 
@@ -92,6 +105,7 @@ app.post("/users", async (req, res) => {
   }
 });
 
+// GET LIST OF ALL ROLES (USED IN ADDING NEW USER)
 app.get("/roles", async (req, res) => {
   let url = "https://www.zohoapis.in/crm/v2/settings/roles";
 
@@ -114,6 +128,7 @@ app.get("/roles", async (req, res) => {
   }
 });
 
+// GET LIST OF ALL PROFILES (USED IN ADDING NEW USER)
 app.get("/profiles", async (req, res) => {
   let url = "https://www.zohoapis.in/crm/v2/settings/profiles";
 
@@ -124,6 +139,88 @@ app.get("/profiles", async (req, res) => {
   let requestDetails = {
     method: "GET",
     headers: headers,
+    throwHttpErrors: false,
+  };
+
+  let response = await got(url, requestDetails);
+
+  if (response != null) {
+    res.send(response.body);
+  } else {
+    res.send("Response empty");
+  }
+});
+
+//GET ALL RECORDS IN A GIVEN MODULE
+// BODY PARAMS
+// 1. module_name (example - "Leads", "Accounts")
+app.get("/records", async (req, res) => {
+  let url;
+
+  if (req.body.id === undefined) {
+    url = `https://www.zohoapis.in/crm/v2/${req.body.module_name}`;
+  } else {
+    url = `https://www.zohoapis.in/crm/v2/${req.body.module_name}/${req.body.id}`;
+  }
+
+  let headers = {
+    Authorization: `Zoho-oauthtoken ${process.env.ACCESS_TOKEN}`,
+  };
+
+  let requestDetails = {
+    method: "GET",
+    headers: headers,
+    throwHttpErrors: false,
+  };
+
+  let response = await got(url, requestDetails);
+
+  if (response != null) {
+    res.send(response.body);
+  } else {
+    res.send("Response empty");
+  }
+});
+
+//Insert a record to a given module
+// Body Params
+//1. module_name (required)
+//2. company
+//3. email
+//4. last_name
+//5. first_name
+//6. lead_status
+
+app.post("/records", async (req, res) => {
+  let url = `https://www.zohoapis.in/crm/v2/${req.body.module_name}`;
+
+  let headers = {
+    Authorization: `Zoho-oauthtoken ${process.env.ACCESS_TOKEN}`,
+  };
+
+  let requestBody = {};
+  let recordArray = [];
+
+  let recordObject = {
+    Company: req.body.company,
+    Email: req.body.email,
+    Last_Name: req.body.last_name,
+    First_Name: req.body.first_name,
+    Lead_Status: req.body.lead_status,
+  };
+
+  recordArray.push(recordObject);
+
+  requestBody["data"] = recordArray;
+
+  let trigger = ["approval", "workflow", "blueprint"];
+  requestBody["trigger"] = trigger;
+
+  let requestDetails = {
+    method: "POST",
+    headers: headers,
+    body: JSON.stringify(requestBody),
+    encoding: "utf8",
     throwHttpErrors: false,
   };
 
